a99153655fdeb9f4f69e8e25c4f1e2e48f2fccae
Jose Antonio Martin <joseant.martin.olmos@gmail.com>
2022-01-06 17:39:34 +0100

    Mas cambios

---
+++ Assets/Scripts/MoveAnimal.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MoveAnimal : MonoBehaviour
+{
+    public virtual void Mueve()
+    {
+        transform.Translate(Vector3.right * 10 * Time.deltaTime);
+    }
+
+    public virtual void GoTo(Vector3 position)
+    {
+        //we don't have a target anymore if we order to go to a random point.
+        /* m_Target = null;
+        m_Agent.SetDestination(position);
+        m_Agent.isStopped = false;
+        */
+    }
+}
